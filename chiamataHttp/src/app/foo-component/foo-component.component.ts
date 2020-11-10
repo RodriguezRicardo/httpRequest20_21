e19957c49d0ce17ab9cba489429b5635c7135cc4
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2020-11-10 08:14:25 +0000

    15.2 finito improving http client

--- chiamataHttp/src/app/foo-component/foo-component.component.ts
+++ chiamataHttp/src/app/foo-component/foo-component.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Foo } from './foo.model';
 
 @Component({
   selector: 'app-foo-component',
@@ -12,11 +13,21 @@ export class FooComponentComponent implements OnInit {
   loading : boolean;        //Una variabile che ci dice se siamo in attesa di una risposta dal server
   o : Observable<Object>;   //Un oggetto che notifica quando arriva la risposta http dal server
 
+  fooData : Foo[];    //Un vettore di oggetti Foo
+  oFoo : Observable<Foo[]>; //Un Observable che riceve un vettore di oggetti Foo
+
   constructor(public http : HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  makeTypedRequest() : void {
+    //oFoo : Observable<Foo[]>; va dichiarato tra gli attributi della classe
+    /*aggiungiamo un metodo che faccia una get ma ritorni un observable
+    che ritorni i dati in una lista di oggetti Foo. (oFoo : Observable<Foo[]>;)*/
+    this.oFoo = this.http.get<Foo[]>('https://jsonplaceholder.typicode.com/posts');
+    this.oFoo.subscribe(data => {this.fooData = data;});
+  }
 
   makeRequest(): void {
     this.loading = true; //Notifichiamo che stiamo attendendo dei dati
